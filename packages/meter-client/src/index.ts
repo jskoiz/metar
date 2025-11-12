727dbad033a1f4e385f68a98d90022b4dee534ab
Jerry <jerry@avmillabs.com>
2025-11-11 18:37:37 -1000

    fix: resolve merge conflict in index.ts - keep both price and MeterClient exports

--- packages/meter-client/src/index.ts
+++ packages/meter-client/src/index.ts
@@ -5,9 +5,6 @@ export * from "./wallet";
 export * from "./payment";
 export * from "./signature";
 export * from "./fetch";
-<<<<<<< HEAD
-=======
 export * from "./price";
 export * from "./MeterClient";
->>>>>>> f993208 (feat(client): implement MeterClient configuration wrapper)
 
